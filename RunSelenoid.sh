75aeb3cb06311c4c53157d24b1d1195ce333cf3c
ekazakova19 <49072165+ekazakova19@users.noreply.github.com>
2020-04-01 11:18:32 +0400

    Final refactor

--- RunSelenoid.sh
+++ RunSelenoid.sh
@@ -1,3 +1,7 @@
+
+# The script deploy selenoid only for MAC OSs
+# The script does not install docker. It is supposed that docker is installed on the machine
+
 #!/bin/bash
  docker ps --filter "name=^selenoid$" --format '{{.Ports}}'
  if [ $? -eq 0 ]
@@ -6,6 +10,7 @@
   exit 127
   else
     open --background -a Docker
+    sleep 5
     curl -s https://aerokube.com/cm/bash | bash \
     && ./cm selenoid start --vnc
     echo "Start selenoid failed" >&2
--- src/test/java/SelenoidDeployment.java
+++ src/test/java/SelenoidDeployment.java
@@ -3,10 +3,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.Console;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class SelenoidDeployment {
     private static final Logger logger = LogManager.getLogger(SelenoidDeployment.class);
@@ -17,7 +14,10 @@ public class SelenoidDeployment {
         processBuilder.command("bash","-c","./RunSelenoid.sh");
 
         try {
-            processBuilder.inheritIO();
+            File deploySelenoidLog = new File("deploySelenoidLog.txt");
+            processBuilder.redirectErrorStream(true);
+            processBuilder.redirectOutput(ProcessBuilder.Redirect.to(deploySelenoidLog));
+
             logger.info("Starting deploy Selenoid");
             Process process = processBuilder.start();
 
